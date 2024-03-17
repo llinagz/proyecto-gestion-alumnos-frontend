169bc526115950877207c7e14aebd4c1724a97d5
Javi Llinares <javierllinaresbarral@outlook.com>
2024-03-17 17:18:07 +0100

    Crear alumnos nuevos. Formulario StudentNew.jsx

--- src/StudentNew.jsx
+++ src/StudentNew.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react"
 import { Header } from "./Header"
+import * as API from "./services/data"
 
 export function StudentNew(){
+
+    const [student, setStudent] = useState({dni:'', nombre:'', direccion:'',edad:'', email:'', asignatura:'1'})
+    
+    function handleSubmit(event){
+        event.preventDefault()
+        API.createStudent(student).then(result =>{
+            if(result =="true"){
+                alert("Alumno insertado con exito")
+                document.getElementById("formulario").reset()
+            }
+            else{
+                alert("Error al insertar el alumno")
+                document.getElementById("formulario").reset()
+            }
+        })
+    }
+
     return(
         <>
             <Header />
-            <p>Nuevo estudiante</p>
+            <p>Nuevo alumno</p>
+            <form id="formulario" onSubmit={handleSubmit}>
+                Dni<input type="text" id="dni" required onChange={event => setStudent({...student, dni:event.target.value})} /> <br />
+                Nombre<input type="text" id="nombre" required onChange={event => setStudent({...student, nombre:event.target.value})} /> <br />
+                Direccion<input type="text" id="direccion" required onChange={event => setStudent({...student, direccion:event.target.value})} /> <br />
+                Edad<input type="number" id="edad" required onChange={event => setStudent({...student, edad:event.target.value})} /> <br />
+                Email<input type="text" id="email" required onChange={event => setStudent({...student, email:event.target.value})} /> <br />
+                Asignatura<select id="asignatura" required onChange={event => setStudent({...student, asignatura:event.target.value})}> 
+                    <option value="1">Matemáticas</option>
+                    <option value="2">Informática</option>
+                    <option value="3">Ingles</option>
+                    <option value="4">Lengua</option>
+                </select>
+                <input type="submit" id="editar" value="Nuevo" />
+            </form>
         </>
     )
 }
--- src/services/data.js
+++ src/services/data.js
@@ -16,10 +16,23 @@ export function login(usuarioLogin, passLogin){
 
 export function getStudents(usuarioProfesor){
     //return fetch(URL + 'alumnosProfesor?usuario=' + usuario)
-    return fetch(`${URL}alumnosProfesor?usuarioProfesor=${usuarioProfesor}`,{
+    return fetch(`${URL}alumnosProfesor?usuarioProfesor=${usuarioProfesor}`, {
         headers:{
             'Content-Type':'application/json'
         }
     })
     .then(data => data.json());
+}
+
+export function createStudent(student){
+    let data = {dni: student.dni, nombre:student.nombre, direccion: student.direccion, edad:student.edad, email:student.email}
+
+    return fetch(`${URL}alumno?idAsignatura=${student.asignatura}`, {
+        method:'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    .then(data => data.text())
 }
